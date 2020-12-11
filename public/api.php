158e48979168ad6e0d206c28eac88f744cd04c20
mie378 <73713814+mie378@users.noreply.github.com>
2020-12-11 20:21:42 +0100

    Add files via upload

--- public/api.php
+++ public/api.php
@@ -13,19 +13,25 @@ class MyDB extends SQLite3 {
  
 
 $app = new \Slim\App;
-$app->get(
-    '/api/participants/{id}',
-    function (Request $request, Response $response, array $args) use ($db) {
-        $sql = "SELECT * FROM participant WHERE id = $args[id]"; // beware! SQL Injection Attack
-        $ret = $db->query($sql);
-        $participant = $ret->fetchArray(SQLITE3_ASSOC);
-        if ($participant) {
-            return $response->withJson($participant);
-        } else {
-            return $response->withStatus(404)->withJson(['error' => 'Such participant does not exist.']);
-        }
-    }
+
+$app->post(
+	'/api/participants',
+	
+	function(Request $request, Response $response, array $args){
+	$requestData = $request->getParsedBody();
+	$db = new MyDB();
+	
+	$imie="'".$requestData['name']."'";
+	$nazw="'".$requestData['surname']."'";
+	if($imie=="''" || $nazw=="''")
+	return $response->withStatus(400);
+	$sql='INSERT INTO participant(firstname,lastname) VALUES('.$imie.','.$nazw.')';
+	$db->query($sql);
+	$db->close();
+	return $response->withStatus(201);
+	}
 );
+
 
 
 $app->run();
